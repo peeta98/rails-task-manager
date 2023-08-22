b83fb4945917f8c4fbd481b088ffae390b6fc716
Pedro Moreira <pedromnmoreira98@gmail.com>
2023-08-22 12:39:18 +0000

    Rails Task Manager challenge complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks', to: 'tasks#index' # Read all the tasks
+  get 'tasks/new', to: 'tasks#new' # The `new` route needs to be placed before the `show` route
+  post 'tasks', to: 'tasks#create' # Creates the new task and displays it on the main page
+  get 'tasks/:id', to: 'tasks#show', as: :task  # Read individual task details
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
